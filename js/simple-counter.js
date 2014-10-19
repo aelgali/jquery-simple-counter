b3b3383473683f6fa060d68cc8f4664fe14c483f
aelgali <aelgali@gmail.com>
2014-10-19 18:28:01 -0500

    Added sample page\n add stop and start fundtion

--- js/simple-counter.js
+++ js/simple-counter.js
@@ -1,4 +1,27 @@
-function Counter() {
-	this.currnentValue = 0;
-	this.startValue = 0;
+function Counter(params) {
+	// {counterHtml, currValue, stValue}
+
+	var counterContainer = $(params['htmlEl']);
+	var startValue = params['startValue'];
+	var maxValue = params['maxValue']? params['maxValue'] : 300;
+	var callback = params['callback']? params['callback'] : null;
+	var currnentValue = startValue;
+	var intervalFun;
+	counterContainer.html(startValue);
+
+	this.start = function() {
+		parentScope = this;
+		intervalFun = setInterval(function() {incrementCounter(callback, parentScope)}, 1000);
+	}
+
+	this.stop = function() {
+		clearInterval(intervalFun);
+	}
+	function incrementCounter (callback, parentScope) {
+		currnentValue = currnentValue + 1;
+		counterContainer.html(currnentValue);
+		if(callback) {
+			callback(parentScope);
+		}
+	}
 }
