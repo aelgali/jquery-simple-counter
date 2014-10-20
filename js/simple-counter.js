ed47a5e817ae9982f2e4b1bea5c3189cb583b30e
aelgali <aelgali@gmail.com>
2014-10-20 13:08:41 -0500

    added reset function

--- js/simple-counter.js
+++ js/simple-counter.js
@@ -1,22 +1,39 @@
 function Counter(params) {
-	// {counterHtml, currValue, stValue}
-
 	var counterContainer = $(params['htmlEl']);
 	var startValue = params['startValue'];
 	var maxValue = params['maxValue']? params['maxValue'] : 300;
 	var callback = params['callback']? params['callback'] : null;
 	var currnentValue = startValue;
 	var intervalFun;
+	var status = 'STOPPED';
+
 	counterContainer.html(startValue);
 
 	this.start = function() {
 		parentScope = this;
 		intervalFun = setInterval(function() {incrementCounter(callback, parentScope)}, 1000);
+		status = 'STARTED';
 	}
 
 	this.stop = function() {
 		clearInterval(intervalFun);
+		status = 'STOPPED';
 	}
+
+	this.reset = function() {
+		if(status === 'STARTED') {
+			this.stop();
+			status = 'STARTED';
+		}
+
+		currnentValue = startValue;
+		counterContainer.html(currnentValue);
+
+		if(status === 'STARTED') {
+			this.start();
+		}
+	}
+
 	function incrementCounter (callback, parentScope) {
 		currnentValue = currnentValue + 1;
 		counterContainer.html(currnentValue);
