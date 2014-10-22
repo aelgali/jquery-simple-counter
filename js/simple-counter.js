955ab48e0e480ad03f3a3b432ead91034e05622c
aelgali <aelgali@gmail.com>
2014-10-21 21:04:39 -0500

    Added setStartValue and getCurrentValue

--- js/simple-counter.js
+++ js/simple-counter.js
@@ -1,15 +1,19 @@
 function Counter(params) {
 	var counterContainer = $(params['htmlEl']);
-	var startValue = params['startValue'];
-	var maxValue = params['maxValue']? params['maxValue'] : 300;
-	var callback = params['callback']? params['callback'] : null;
+	var startValue = params['startValue'] ? params['startValue'] : 0;
+	var maxValue = params['maxValue'] ? params['maxValue'] : 300;
+	var callback = params['callback'] ? params['callback'] : null;
 	var currnentValue = startValue;
 	var intervalFun;
 	var status = 'STOPPED';
-
 	counterContainer.html(startValue);
 
 	this.start = function() {
+		if(status === 'STARTED') {
+			return;
+		} else {
+			currnentValue = startValue;
+		}
 		parentScope = this;
 		intervalFun = setInterval(function() {incrementCounter(callback, parentScope)}, 1000);
 		status = 'STARTED';
@@ -30,10 +34,19 @@ function Counter(params) {
 		counterContainer.html(currnentValue);
 
 		if(status === 'STARTED') {
+			status = 'RESETED';
 			this.start();
 		}
 	}
 
+	this.setStartValue = function(newStartValue) {
+		startValue = parseInt(newStartValue);
+	}
+
+	this.getCurrentValue = function() {
+		return currnentValue;
+	}
+	
 	function incrementCounter (callback, parentScope) {
 		currnentValue = currnentValue + 1;
 		counterContainer.html(currnentValue);
